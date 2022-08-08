fa8c7831a9caecfa75992b842c9717ee8907657c
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-08 21:08:44 +0300

    add task-6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#validation-input');
+const data = inputEl.getAttribute('data-length');
+
+inputEl.onblur = () => {
+  if (inputEl.value.length === Number(data)) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else if (inputEl.value.length === 0) {
+    inputEl.classList.remove('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
+  }
+};
