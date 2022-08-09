5a9a86b667e4839315a1c0c527d964819cb58cec
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-09 23:12:52 +0300

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('#validation-input');
 const data = inputEl.getAttribute('data-length');
 
 inputEl.onblur = () => {
-  if (inputEl.value.length === Number(data)) {
+  if (inputEl.value.length === +data) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
   } else if (inputEl.value.length === 0) {
